cba9dbac3cd4c9945310be154ad60130ffc80ceb
Colin Farquhar <cifarquhar@gmail.com>
2017-09-30 13:02:22 +0100

    add Game container

---
+++ src/containers/Game.js
@@ -0,0 +1,24 @@
+import React, {Component} from "react"
+import {
+  StyleSheet,
+  View,
+  Text
+} from "react-native"
+
+class Game extends Component {
+
+  constructor(props){
+    super(props)
+  }
+
+  render(){
+    return(
+      <View>
+        <Text>Game stuff goes in here</Text>
+      </View>
+    )
+  }
+
+}
+
+export default Game
