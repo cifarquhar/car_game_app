aaad28f7cd5db91613644986ab0a1764f746b99a
Colin Farquhar <cifarquhar@gmail.com>
2017-10-01 16:06:08 +0100

    add answer buttons to game view

--- src/containers/Game.js
+++ src/containers/Game.js
@@ -1,7 +1,7 @@
 import React, {Component} from "react"
 import {
   StyleSheet,
-  View,
+  ScrollView,
   Text,
   Button
 } from "react-native"
@@ -10,14 +10,48 @@ import { StackNavigator } from "react-navigation"
 class Game extends Component {
 
   static navigationOptions = ({navigation}) => ({
-    title: "<-- name goes here -->",
+    title: navigation.state.params.gameType,
   })
 
   render(){
+
+    let buttonNames = []
+
+    switch (this.props.navigation.state.params.gameType){
+      case "Car Colours":
+        buttonNames = ["Black", "White", "Red", "Yellow", "Blue", "Green", "Silver"]
+        break
+      case "Car Types":
+        buttonNames = ["Volvo", "Skoda", "Volkswagen", "Ford", "Aston Martin", "Ferrari", "Porsche", "Lamborghini", "Audi", "Smart", "Peugeot", "Renault", "Honda", "Mercedes", "Infiniti", 
+                      "Vauxhall", "Nissan", "Toyota", "BMW", "Jaguar", "Hyundai", "Citroen"]
+        break
+      case "Supermarkets":
+        buttonNames = ["Waitrose", "Tesco", "Sainsbury's", "Co-op", "Morrisons", "Asda", "Lidl", "Aldi", "M&S Food", "Iceland"]
+        break
+      case "Delivery Companies": 
+        buttonNames = ["Royal Mail", "Parcel Force", "DPD", "TNT", "Hermes", "Yodel", "Reed Bordall", "Tufnell", "UPS", "DHL", "FedEx", "UK Mail", "DX", "P&O"]
+        break
+      case "Lorries":
+        buttonNames = ["Next", "Maersk", "Kuhne+Nagel", "Nisa", "Argos", "Debenhams", "John Lewis", "House of Fraser", "Boots", "Pollock", "JBT", "FreshLinc", "Currie", "Eddie Stobart", "M&S"]
+        break
+      default:
+        buttonNames = []
+    }
+
+    let buttons = buttonNames.map((name,index) => {
+      return(
+        <Button 
+          key={index}
+          onPress={() => console.log(name + " pressed")}
+          title={name}
+        />
+      )
+    })
+
     return(
-      <View>
-        <Text>Game buttons go in here</Text>
-      </View>
+      <ScrollView>
+        {buttons}
+      </ScrollView>
     )
   }
 
